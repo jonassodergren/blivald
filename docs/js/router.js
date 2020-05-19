e6b587d20a68c348c0f9607026ea726545bf108b
jonassodergren <jonas.sodergren@gmail.com>
2020-05-19 17:06:58 +0200

    Added verify.html

--- docs/js/router.js
+++ docs/js/router.js
@@ -7,8 +7,9 @@ var loc = window.location.href;
 loc = loc.lastIndexOf('/') == loc.length - 1 ? loc.substr(0, loc.length - 1) : loc.substr(0, loc.length + 1);
 var pathname = loc.substr(loc.lastIndexOf('/') + 1);
 
-if (pathname === "verify")
+if (pathname === "verify"){
 url = url + 'verify.html';
+}
 else{
   url = url + 'error.html?errorCode=404';
 }
