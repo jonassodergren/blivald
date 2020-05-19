80fa12477f589f6ff762050567166dc60539661a
jonassodergren <jonas.sodergren@gmail.com>
2020-05-19 22:01:58 +0200

    Added verify.html

--- docs/js/verify.js
+++ docs/js/verify.js
@@ -1,3 +1,7 @@
 window.onload = async () => {
+
+  let params = (new URL(document.location)).searchParams;
+  let id = params.get('id');
+  document.getElementById("certificate_id").value = id;
     window.history.replaceState({}, document.title, "/verify/");
 };
