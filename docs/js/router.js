1f4e1ebc8b0d2228a8eff04b67e2f8ee2653c333
jonassodergren <jonas.sodergren@gmail.com>
2020-05-19 16:55:59 +0200

    Added verify.html

--- docs/js/router.js
+++ docs/js/router.js
@@ -1,14 +1,16 @@
 
 (function(){
 
+var url = "https://www.blivald.se/"
+
 var loc = window.location.href;
 loc = loc.lastIndexOf('/') == loc.length - 1 ? loc.substr(0, loc.length - 1) : loc.substr(0, loc.length + 1);
 var pathname = loc.substr(loc.lastIndexOf('/') + 1);
 
 if (pathname === "verify")
-url = 'https://www.blivald.se/' + 'verify.html';
+url = url + 'verify.html';
 else{
-  url = 'https://www.blivald.se/error.html?errorCode=404'
+  url = url + 'error.html?errorCode=404';
 }
 
 location.href = url;
