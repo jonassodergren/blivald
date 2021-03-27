235c5c71a974a34e956452768a970ab901553848
Jonas <jonas.sodergren@gmail.com>
2021-03-27 22:02:55 +0100

    Added step four

--- docs/js/create.js
+++ docs/js/create.js
@@ -1,36 +1,4 @@
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-//$(document).ajaxStart(function(){
-  // set overlay
-//  var overlayElement = $('#selectCompany');
-//  overlayElement.LoadingOverlay("show", {
-//    background  : "rgba(92, 184, 92, 0.4)",
-//    image       : ""
-//    text        : "Snart klar!..."
-//  });
-//});
-
-//}, false);
 
 $(document).on("adduserinfo", function(e, email, api_key){
     //var email = document.getElementById("email").value;
